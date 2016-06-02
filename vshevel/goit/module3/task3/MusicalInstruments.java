84e528f6709390114bf8afb9e76e59e6b469e5ff
VladimirShevel <v.shevel@gmail.com>
2016-06-02 23:03:14 +0300

    poly

--- vshevel/goit/module3/task3/MusicalInstruments.java
+++
@@ -1,9 +0,0 @@
-package vshevel.goit.module3.task3;
-
-public class MusicalInstruments {
-
-    public  void howDoesThisSound(){
-        //как это звучит
-        System.out.println("It's sound like musikal instrument");
-    }
-}
