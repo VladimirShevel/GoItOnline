50896064bc903262f49ca3d9334ffd837a9377cf
VladimirShevel <v.shevel@gmail.com>
2016-06-16 03:31:20 +0300

    Instrument modified

--- vshevel/goit/module6/task2/Instrument.java
+++ vshevel/goit/module6/task2/Instrument.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class Instrument {
-    public final static String PIANO = "Piano";
-    public static final String GUITAR = "Guitar";
-    public final static String TRUMPET = "Trampet";
+    public final static String PIANO = "piano";
+    public static final String GUITAR = "guitar";
+    public final static String TRUMPET = "Trumpet";
           }
