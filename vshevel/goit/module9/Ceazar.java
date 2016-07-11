8128b0f60ed91fdb6b96a3f99aa28ba8e1bfcf32
VladimirShevel <v.shevel@gmail.com>
2016-07-11 23:58:46 +0300

    Ceazar.encrypt fixed

--- vshevel/goit/module9/Ceazar.java
+++ vshevel/goit/module9/Ceazar.java
@@ -7,18 +7,18 @@ public class Ceazar {
 
 
     public static String encrypt(int key, String inputString) {
-        ArrayList<char[]> charArray = new ArrayList<>();
+        ArrayList<Character> charArray = new ArrayList<>();
         for (int i = 0; i < inputString.length(); i++) {
             int a = ((int) inputString.charAt(i) + key);
-            charArray.add(Character.toChars(a));
+            charArray.add((char) a);
         }
         StringBuilder builder = new StringBuilder();
-        for (char[] value : charArray) {
+        for (Character value : charArray) {
             builder.append(value);
         }
-            return builder.toString();
+        return builder.toString();
     }
-    public static String decrypt (int key, String inputString){
+    public static String decrypt (int key, String inputString) {
         return encrypt((-key), inputString);
     }
 }
